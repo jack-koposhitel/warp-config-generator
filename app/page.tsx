2afe161f0a7d8e717432d01db96703005c907399
jack-koposhitel <q8spogaj9oyh@abinemail.com>
2025-08-15 14:41:13 +0300

    Style logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Analytics/>
       <main className="flex min-h-screen flex-col items-center justify-center p-4 w-full">
         <div className="flex flex-col items-center justify-center gap-6 w-[300px]">
-          <h1>Amnezia <span className="warp-logo">+&nbsp;WARP</span></h1>
+          <h1 className="logo">Amnezia <span className="warp-logo">+&nbsp;WARP</span></h1>
           <WarpGenerator/>
         </div>
       </main>
