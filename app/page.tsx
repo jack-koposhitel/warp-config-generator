f92ebc26d1cc8d513da13ee70e5378cb3668261a
jack-koposhitel <q8spogaj9oyh@abinemail.com>
2025-08-15 14:35:00 +0300

    Cleanup main page, replace logo with text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
 import { WarpGenerator } from "@/components/warp-generator"
 import { Analytics } from "@vercel/analytics/next"
@@ -10,7 +9,7 @@ export default function Home() {
       <Analytics/>
       <main className="flex min-h-screen flex-col items-center justify-center p-4 w-full">
         <div className="flex flex-col items-center justify-center gap-6 w-[300px]">
-          <Image src="/logo.svg" alt="Логотип" width={300} height={300}/>
+          <h1>Amnezia <span className="warp-logo">+&nbsp;WARP</span></h1>
           <WarpGenerator/>
         </div>
       </main>
