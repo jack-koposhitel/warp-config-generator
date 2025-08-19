14852ac9e7ebfb6ee69875891c7e174d8c4d82d1
jack-koposhitel <q8spogaj9oyh@abinemail.com>
2025-08-20 00:56:39 +0300

    Fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -18,25 +18,25 @@ export default function Home() {
       <Analytics/>
       <main className="flex min-h-screen flex-col items-center justify-center p-4 w-full">
         <div className="flex flex-col items-center justify-center gap-6 w-[300px]">
-          <Heading className="logo">Amnezia <Text as="span" className="warp-logo">+&nbsp;WARP</Text></Heading>
+          <h1 className="logo">Amnezia <span className="warp-logo">+&nbsp;WARP</span></h1>
           <WarpGenerator/>
-          <Flex gap="2">
-            <Button asChild variant="soft">
-              <Link target="_blank" href="https://telegra.ph/Amnezia--WARP-08-19"><BookOpenText /> Инструкция</a>
+          <div className="flex gap-2">
+            <Button asChild variant="secondary">
+              <a href="https://telegra.ph/Amnezia--WARP-08-19" target="_blank"><BookOpenText /> Инструкция</a>
             </Button>
-            <Button asChild variant="soft">
-              <Link target="_blank" href="https://github.com/user-attachments/assets/d2b87e56-f7ff-429e-b49c-07dbd798a37a"><Play /> Видео</a>
+            <Button asChild variant="secondary">
+              <a href="https://github.com/user-attachments/assets/d2b87e56-f7ff-429e-b49c-07dbd798a37a" target="_blank"><Play /> Видео</a>
             </Button>
-          </Flex>
-          <Flex gap="2">
-            <Button asChild variant="soft">
-              <Link target="_blank" href="{repoUrl}/discussions"><MessageCircle/> Обсудить</a>
+          </div>
+          <div className="flex gap-2">
+            <Button asChild variant="secondary">
+              <a href="{repoUrl}/discussions" target="_blank"><MessageCircle/> Обсудить</a>
             </Button>
-            <Button asChild variant="soft">
-              <Link target="_blank" href="{repoUrl}/issues"><Bug/> Сообщить об ошибке</a>
+            <Button asChild variant="secondary">
+              <a href="{repoUrl}/issues" target="_blank"><Bug/> Сообщить об ошибке</a>
             </Button>
-          </Flex>
-          <Link target="_blank" href="{repoUrl}/issues"><Code/> Исходный код</a>
+          </div>
+          <a href="{repoUrl}" target="_blank"><Code/> Исходный код</a>
         </div>
       </main>
     </>
