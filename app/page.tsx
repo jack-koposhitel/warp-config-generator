287ab3177499530bb66048c97debb083db8c4b5b
jack-koposhitel <q8spogaj9oyh@abinemail.com>
2025-08-19 03:37:13 +0300

    Add video

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Button } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { WarpGenerator } from "@/components/warp-generator"
 import { Analytics } from "@vercel/analytics/next"
@@ -11,6 +12,16 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center gap-6 w-[300px]">
           <h1 className="logo">Amnezia <span className="warp-logo">+&nbsp;WARP</span></h1>
           <WarpGenerator/>
+          	<Accordion.Root type="single" collapsible>
+              <Accordion.Item className="AccordionItem" value="item-1">
+                <AccordionTrigger>Видео</AccordionTrigger>
+                <AccordionContent>
+                  <video width="300" controls>
+                    <source src="https://github.com/user-attachments/assets/86df4274-9383-46bf-b9ea-4427ca021e9a" type="video/mp4"/>
+                  </video>
+                </AccordionContent>
+              </Accordion.Item>
+            </Accordion.Root>
         </div>
       </main>
     </>
